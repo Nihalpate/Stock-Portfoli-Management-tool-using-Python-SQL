ddf3980ec878fcc7b47c5617f920143dc03c0540
Nihal Patel <npate88@stevens.edu>
2022-07-04 16:16:58 -0400

    completed python and sql connection

--- q_1.sql
+++ q_1.sql
@@ -1,3 +1,3 @@
 SELECT 
 	*
-FROM Tableau_Table_4
+FROM 
--- sql_connector.py
+++ sql_connector.py
@@ -1,15 +1,29 @@
-import mysql.connector
+from sympy import re
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    password = "Nihal@301197",
-    database = "nihal"
- )
 
-#nihal = mydb.cursor()
-#nihal.execute()
+def sql_runner (file_name):
 
-file = open("q_1.sql","r")
-for i in file:
-    print(i)
+    import mysql.connector
+    from pathlib import Path
+
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "root",
+        password = "Nihal@301197",
+        database = "nihal"
+    )
+    # to find the relative path for perticular filename in the current directory
+    filepath = Path(__file__).parent / file_name
+    print("file path: " + str(filepath))
+    filename = open(filepath)
+    text = (filename.read())
+
+    nihal = mydb.cursor()
+    nihal.execute(text + "tableau_table_4")
+
+    for i  in nihal:
+        print(i)
+
+
+
+sql_runner("q_1.sql")
